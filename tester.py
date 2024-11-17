95b1198d929e7dc3d7fb51229340a329a118ca76
nohyewon <yw4913@naver.com>
2024-11-17 13:11:43 +0900

    error test11

--- tester.py
+++ tester.py
@@ -1,11 +1,9 @@
 import geo.utils as utils
 
-#calculate the length of c
-
 a, b = 3, 4
 c = utils.pythagoras(a, b)
-print('c = ', c)
+print('c =', c)
 
 r = 10
 area = utils.circle(r)
-print('area = ', area)
+print('area =', area)
